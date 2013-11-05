96f3b432523216c55860abc91bd0b5aa319f0e76
nonaka9682 <nonaka9862@WhiteBook.local>
2013-11-05 10:23:50 +0900

    Initial Commit

---
+++ ImageView1/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ImageView1
+//
+//  Created by HirokiNonaka on 2013/11/05.
+//  Copyright (c) 2013年 HirokiNonaka. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ImageView1/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ImageView1
+//
+//  Created by HirokiNonaka on 2013/11/05.
+//  Copyright (c) 2013年 HirokiNonaka. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
